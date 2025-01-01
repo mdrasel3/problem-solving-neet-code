2745553e2b5aad2ee3501549fe440678574d3174
Md Rasel <95276609+mdrasel3@users.noreply.github.com>
2025-01-01 18:58:07 +0100

    checking valid anagram using hashmap in python

---
+++ md_rasel_py_solutions/valid_anagram_v1.py
@@ -0,0 +1,28 @@
+"""
+Created on Wed Jan  1 18:34:04 2025
+
+@author: Md Rasel
+"""
+# Problem Statement: Given two strings `s` and `t`, return `true` if 
+# the two strings are anagrams of each other, otherwise return `false`.
+# An anagram is a string that contains the exact same characters as
+# another string, but the order of the characters can be different.
+
+# Soln approach: check if the length of two strings is equal or not
+# 
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # if the length of s and t is not equal, not anagram
+        if len(s) != len(t): return False 
+        # frequency dict for both strings 
+        freq_s = {} 
+        freq_t = {} 
+        # dict get(key,default) method 
+        for char in s: 
+            freq_s[char] = freq_s.get(char, 0) + 1 
+        for char in t: 
+            freq_t[char] = freq_t.get(char, 0) + 1 
+        
+        # compare the frequency dict, if true -> anagram
+        return freq_s == freq_t
